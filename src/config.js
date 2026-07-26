7414921d67f1fd9f5062c4e09f2b7d7854f70d0d
permabulk69420-pixel <permabulk69420@gmail.com>
2026-07-27 01:59:23 +1000

    Add central world and asset configuration

---
+++ src/config.js
@@ -0,0 +1,22 @@
+export const SCALE = 0.3048 * 1.5;
+
+export const HOUSE = Object.freeze({
+  width: 33 * SCALE,
+  depth: 32 * SCALE,
+  porchDepth: 5 * SCALE,
+  wallHeight: 2.72,
+  wallThickness: 0.17,
+  slabHeight: 0.14,
+  playerRadius: 0.25
+});
+
+export const ASSETS = Object.freeze({
+  leftHand: './assets/models/hands/LeftHand.glb',
+  rightHand: './assets/models/hands/RightHand.glb',
+  computerDesk: './assets/models/furniture/ComputerDesk.glb'
+});
+
+export const PLAYER = Object.freeze({
+  moveSpeed: 2.25,
+  turnSpeed: 1.9
+});
