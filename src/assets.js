f4d7c32a5f5c6e5cd8a26e8ddeb0ed74505d3a6b
permabulk69420-pixel <permabulk69420@gmail.com>
2026-07-27 23:19:10 +1000

    Bump the corrected bat grip module

--- src/assets.js
+++ src/assets.js
@@ -7,7 +7,7 @@ import { registerSlidingDeskInteractions } from './interactions/sliding-grab.js'
 import { loadBedroomBed } from './furniture/bed-setup.js?v=1';
 import { loadBedsideSetup } from './furniture/bedside-setup.js?v=2';
 import { loadEntertainmentSetup } from './furniture/entertainment-setup.js?v=2';
-import { loadWoodenBat } from './weapons/wooden-bat.js?v=2';
+import { loadWoodenBat } from './weapons/wooden-bat.js?v=3';
 
 export async function loadDecorAssets({
   scene,
