6aae708a355d108eb9c2e791bb00748f431fc58a
permabulk69420-pixel <permabulk69420@gmail.com>
2026-07-29 06:52:24 +1000

    Refresh skitter lighting integration

--- src/assets-combat.js
+++ src/assets-combat.js
@@ -1,5 +1,5 @@
 import { loadDecorAssets as loadBaseDecorAssets } from './assets.js?base=1';
-import { loadSkitterEnemy } from './enemies/skitter-enemy-facing.js?v=3';
+import { loadSkitterEnemy } from './enemies/skitter-enemy-facing.js?v=4';
 
 export async function loadDecorAssets(options) {
   const decor = await loadBaseDecorAssets(options);
